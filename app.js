905250c08a4389b173f1a478aef55deca0c78bdf
Mesut Ozturk <doktormesut@gmail.com>
2023-07-27 22:47:07 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,4 @@
+let definitionText =document.getElementById("definition-text");
+let wordText =document.getElementById("definition-text") ;
+let url ="https://api.dictionaryapi.dev/api/v2/entries/en/";
+let btn =document.getElementById("btn")
